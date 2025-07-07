33ee787dce47d1b02cff3a6d215e8e2c8bddd069
saskia-lawrence <saskialawrence02@gmail.com>
2025-07-07 12:08:54 +0530

    commited by saskia
    sortout small error

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <h1>jdfjljd</h1>
+    <App />
   </React.StrictMode>
 );
 
